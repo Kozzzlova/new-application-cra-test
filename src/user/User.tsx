fee828176596e3d9bd8c861ce08197e414af7f72
Kozzzlova <kozlova.jully@gmail.com>
2024-06-21 12:14:54 +0300

    developer/popup branch commit 2

--- src/user/User.tsx
+++ src/user/User.tsx
@@ -8,6 +8,8 @@ export const User = () => {
          <p>Dev branch commit 1</p>
          <p>Dev branch commit 2</p>
          <p>Dev branch commit 3</p>
+         <p>developer/popup branch commit 1</p>
+         <p>developer/popup branch commit 2</p>
       </div>
    );
 };
